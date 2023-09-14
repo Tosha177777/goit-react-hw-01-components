545a885b5192241b2975efa6e224d6f1dbfb72c1
Tosha177777 <slavkoanton4@gmail.com>
2023-09-14 16:09:29 +0300

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
-export const App = () => {
+import Profile from './Profile';
+
+const App = () => {
   return (
     <div
       style={{
@@ -7,10 +9,18 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
+
+export default App;
